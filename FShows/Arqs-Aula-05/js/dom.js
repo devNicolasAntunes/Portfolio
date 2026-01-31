b173683beb6be5fa7c9af4393bdb08407879e9e9
Bulletsz <160187242+Bulletsz@users.noreply.github.com>
2026-01-31 20:07:06 -0300

    Add files via upload

---
+++ FShows/Arqs-Aula-05/js/dom.js
@@ -0,0 +1,17 @@
+/*pega elementos da página por ID
+const getnomeBanda = document.getElementById("nomeBanda");
+console.log(getnomeBanda);
+
+pega apenas o primeiro elemento que ele encontra e ignora o resto.
+pode-se usar o (queryselectorALL) que ele pega todos os elementos encontrados
+const getformShow = document.querySelector("#formShow");
+console.log(getformShow);
+
+//pega elementos da página por classes CSS
+const getTheadShows = document.getElementsByClassName("thead-shows");
+console.log(getTheadShows);
+
+//pega elementos pelo nome da tag
+const getDiv = document.getElementsByTagName("div")
+console.log(getDiv);*/
+
