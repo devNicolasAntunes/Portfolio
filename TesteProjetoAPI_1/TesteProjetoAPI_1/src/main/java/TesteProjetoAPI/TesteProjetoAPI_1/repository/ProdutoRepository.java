deb37639347f1dbdb3e11a1b275a1d8b9275b4a9
Nícolas Antunes <160187242+devNicolasAntunes@users.noreply.github.com>
2026-03-09 13:14:44 -0300

    Delete TesteProjetoAPI_1 directory

--- TesteProjetoAPI_1/TesteProjetoAPI_1/src/main/java/TesteProjetoAPI/TesteProjetoAPI_1/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package TesteProjetoAPI.TesteProjetoAPI_1.repository;
-
-import TesteProjetoAPI.TesteProjetoAPI_1.model.ProdutoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<ProdutoEntity, Long> {
-}
